500192a53e8fe5297e8a1f001b6224e8c9ae42d4
alordash <alordash@mail.ru>
2021-09-24 13:42:05 +0300

    Added binary power

---
+++ FastExponentiation/FastExponentiation/FastMath.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FastExponentiation {
+	public static class FastMath {
+		public static double FastPower(double b, int e) {
+			double v = 1;
+			while(e > 0) {
+				if((e & 1) == 1) {
+					v *= b;
+				}
+				e >>= 1;
+				b *= b;
+			}
+			return v;
+		}
+		public static double FastPower(double b, double e) {
+			return FastPower(b, (int)e);
+		}
+	}
+}
