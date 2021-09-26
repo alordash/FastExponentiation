4a5db588a8dd808c28fa6b84af09c7229b9ac10b
alordash <alordash@mail.ru>
2021-09-27 00:29:15 +0300

    Added approximate and real values comparison chart

--- FastExponentiation/FastExponentiation/FastMath.cs
+++ FastExponentiation/FastExponentiation/FastMath.cs
@@ -5,7 +5,7 @@ using System.Threading.Tasks;
 
 namespace FastExponentiation {
 	public static class FastMath {
-		public static double BinaryPower(double b, long e) {
+		private static double BinaryPower(double b, long e) {
 			double v = 1;
 			while(e > 0) {
 				if((e & 1) == 1) {
@@ -17,8 +17,8 @@ namespace FastExponentiation {
 			return v;
 		}
 
-		public static long doubleApproximator = 4606853616395542500L;
-		public static double FastApproximatePower(double b, double e) {
+		private static long doubleApproximator = 4606853616395542500L;
+		private static double FastApproximatePower(double b, double e) {
 //			long k = (long)((1L << 52) * ((1L << 10) - 1.0730088));
 			long i = BitConverter.ToInt64(BitConverter.GetBytes(b));
 			i = (long)(FastMath.doubleApproximator + e * (i - FastMath.doubleApproximator));
