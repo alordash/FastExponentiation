4531743fb9e56b7991ae0db0850fc81ff317264d
alordash <alordash@mail.ru>
2021-09-27 22:03:14 +0300

    Added basic benchmark for comparing methods

--- FastExponentiation/FastExponentiationBenchmark/Program.cs
+++ FastExponentiation/FastExponentiationBenchmark/Program.cs
@@ -1,10 +1,46 @@
 ﻿using System;
-using FastMath;
+using System.Diagnostics;
 
 namespace FastExponentiationBenchmark {
 	class Program {
 		static void Main(string[] args) {
-			Console.WriteLine("Hello World!");
+			while(true) {
+				int count;
+				Console.WriteLine("Enter count:");
+				while(!int.TryParse(Console.ReadLine(), out count)) {
+					Console.WriteLine("Entered wrong count");
+				}
+
+				double Exp;
+				Console.WriteLine("Enter exponent:");
+				while(!double.TryParse(Console.ReadLine(), out Exp)) {
+					Console.WriteLine("Entered wrong exponent");
+				}
+
+				var random = new Random();
+				var bases = new double[count];
+				for(int i = 0; i < count; i++) {
+					bases[i] = random.NextDouble();
+				}
+
+				var realValues = new double[count];
+				var approximateValues = new double[count];
+				var stopWatch = new Stopwatch();
+				stopWatch.Start();
+				for(int i = 0; i < count; i++) {
+					realValues[i] = Math.Pow(bases[i], Exp);
+				}
+				stopWatch.Stop();
+				Console.WriteLine(String.Format("Traditional method took {0}ms for raising {1} numbers to power {2}", stopWatch.ElapsedMilliseconds, count, Exp));
+				stopWatch.Reset();
+
+				stopWatch.Start();
+				for(int i = 0; i < count; i++) {
+					approximateValues[i] = FastMath.FastPower(bases[i], Exp);
+				}
+				stopWatch.Stop();
+				Console.WriteLine(String.Format("Approximate method took {0}ms for raising {1} numbers to power {2}", stopWatch.ElapsedMilliseconds, count, Exp));
+			}
 		}
 	}
 }
