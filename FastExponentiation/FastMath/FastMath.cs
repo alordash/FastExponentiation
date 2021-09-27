a292336a59f9b9b51b1fc1f0eb65a0bf9b954316
alordash <alordash@mail.ru>
2021-09-27 22:03:02 +0300

    Removed namespace in FastMath class

--- FastExponentiation/FastMath/FastMath.cs
+++ FastExponentiation/FastMath/FastMath.cs
@@ -1,34 +1,32 @@
 ﻿using System;
 
-namespace FastMath {
-	public static class FastMath {
-		public static double BinaryPower(double b, long e) {
-			double v = 1;
-			while(e > 0) {
-				if((e & 1) == 1) {
-					v *= b;
-				}
-				e >>= 1;
-				b *= b;
+public static class FastMath {
+	public static double BinaryPower(double b, long e) {
+		double v = 1;
+		while(e > 0) {
+			if((e & 1) == 1) {
+				v *= b;
 			}
-			return v;
+			e >>= 1;
+			b *= b;
 		}
+		return v;
+	}
 
-		public static long doubleApproximator = 4606853616395542500L;
-		public static double FastApproximatePower(double b, double e) {
-			//			long k = (long)((1L << 52) * ((1L << 10) - 1.0730088));
-			unsafe {
-				long i = *(long*)&b;
-				i = (long)(FastMath.doubleApproximator + e * (i - FastMath.doubleApproximator));
-				b = *(double*)&i;
-			}
-			return b;
+	public static long doubleApproximator = 4606853616395542500L;
+	public static double FastApproximatePower(double b, double e) {
+		//			long k = (long)((1L << 52) * ((1L << 10) - 1.0730088));
+		unsafe {
+			long i = *(long*)&b;
+			i = (long)(FastMath.doubleApproximator + e * (i - FastMath.doubleApproximator));
+			b = *(double*)&i;
 		}
+		return b;
+	}
 
-		public static double FastPower(double b, double e) {
-			var el = (long)Math.Ceiling(Math.Abs(e));
-			var basePart = FastApproximatePower(b, e / el);
-			return BinaryPower(basePart, el);
-		}
+	public static double FastPower(double b, double e) {
+		var el = (long)Math.Ceiling(Math.Abs(e));
+		var basePart = FastApproximatePower(b, e / el);
+		return BinaryPower(basePart, el);
 	}
 }
