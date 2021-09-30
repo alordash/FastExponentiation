fba1b6218ee631442212cf491032a3179d4bb019
alordash <alordash@mail.ru>
2021-09-30 13:47:16 +0300

    Improved FastExponentiationCpp formatting

--- FastExponentiation/FastExponentiationCpp/FastExponentiationCpp.cpp
+++ FastExponentiation/FastExponentiationCpp/FastExponentiationCpp.cpp
@@ -3,29 +3,42 @@
 #include <iomanip>
 #include "FastMath.h"
 
+#define _NUM_IS_UNREAL(x) std::isnan(x) || !std::isfinite(x)
+#define _FORMAT_ERROR_S(x) (_NUM_IS_UNREAL(x) ? "\033[31;7m" : (x > 25 ? "\033[31m" : (x > 10 ? "\033[33m" : "\033[32m")))
+#define _DEFAULT "\033[0m"
+#define _FORMAT_ERROR(x) _FORMAT_ERROR_S(x) << x << _DEFAULT
 #define DIFFERENCE_BAR_WIDTH 100
 
+#define _WIDTH 16
+#define _SETW std::setw(_WIDTH)
+#define _L std::left
+#define _R std::right
+#define _FILL(x) std::setfill(x)
+
 void ShowDifference(double percentageDifference) {
-	int fillAmount = (int)round((double)DIFFERENCE_BAR_WIDTH * percentageDifference / 100.0);
-	bool exceededLimit = fillAmount > DIFFERENCE_BAR_WIDTH;
-	std::cout << "Difference: " << (exceededLimit ? "\033[31m" : "") << percentageDifference << (exceededLimit ? "\033[0m" : "") << "%\n";
+	int fillAmount = 0;
+	bool differenceIsNan = false;
+	std::cout << _R;
+	if(_NUM_IS_UNREAL(percentageDifference)) {
+		fillAmount = DIFFERENCE_BAR_WIDTH;
+		differenceIsNan = true;
+	} else {
+		fillAmount = (int)round((double)DIFFERENCE_BAR_WIDTH * percentageDifference / 100.0);
+	}
+	std::cout << "Difference: " << _FORMAT_ERROR(percentageDifference) << "%\n";
 
 	// top cap
 	std::cout << char(218) << std::setfill(char(196)) << std::setw(DIFFERENCE_BAR_WIDTH + 1) << char(191) << "\n";
 	// left border
 	std::cout << char(179);
 
-	if(exceededLimit) {
+	if(fillAmount > DIFFERENCE_BAR_WIDTH) {
 		fillAmount = DIFFERENCE_BAR_WIDTH;
 	}
 	int emptinessAmount = DIFFERENCE_BAR_WIDTH - fillAmount;
 
-	if(exceededLimit) {
-		// red color if difference > 100%
-		std::cout << "\033[31m";
-	}
 	// difference filler
-	std::cout << std::setfill('#') << std::setw(fillAmount + 4) << "\033[0m";
+	std::cout << _FORMAT_ERROR_S(percentageDifference) << (differenceIsNan ? _FILL('?') : _FILL('#')) << std::setw(fillAmount + 4) << _DEFAULT;
 
 	// emptiness filler
 	std::cout << std::setfill(' ') << std::setw(emptinessAmount + 1) << char(179) << '\n';
@@ -37,14 +50,14 @@ void ShowDifference(double percentageDifference) {
 int main() {
 	while(true) {
 		double base, exponent;
-		std::cout << "Enter base:\n";
+		std::cout << _L << _SETW << _FILL(' ') << "Enter base:";
 		std::cin >> base;
-		std::cout << "Enter exponent:\n";
+		std::cout << _L << _SETW << _FILL(' ') << "Enter exponent:";
 		std::cin >> exponent;
 		double realValue = pow(base, exponent);
 		double approximateValue = FastMath::FastPower(base, exponent);
-		std::cout << "Real result: " << realValue << "\n";
-		std::cout << "Approximate result: " << approximateValue << "\n";
+		std::cout << _L << _SETW << _FILL(' ') << "Real result: " << realValue << "\n";
+		std::cout << _L << _SETW << _FILL(' ') << "Approximate: " << approximateValue << "\n";
 		double percentageDifference = FastMath::ToPercentage(realValue / approximateValue);
 
 		ShowDifference(percentageDifference);
