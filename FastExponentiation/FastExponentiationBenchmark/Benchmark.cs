c1376ae8697225aa32b784fec335d32970a1bd71
alordash <alordash@mail.ru>
2021-10-19 21:21:55 +0300

    Renamed Program.cs

---
+++ FastExponentiation/FastExponentiationBenchmark/Benchmark.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Diagnostics;
+using BenchmarkDotNet.Running;
+
+namespace FastExponentiationBenchmark {
+	class Benchmark {
+		static void Main(string[] args) {
+			BenchmarkRunner.Run<GeneralBenchmark>();
+		}
+	}
+}
